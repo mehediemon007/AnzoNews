031dda88fec492714917a059fc8d974ad0a2c5e8
mehediemon007 <mehediemon7@gmail.com>
2022-03-08 19:01:25 +0600

    fixing

--- src/components/grid-blogs/GridBlogs.jsx
+++ src/components/grid-blogs/GridBlogs.jsx
@@ -5,14 +5,12 @@ import SingleBlog from './single-blog/SingleBlog';
 function GridBlogs() {
 
     return (
-        <div>
-            <div className="container">
-                <h2 className="mb-3">Categories</h2>
-                <div className="blog-category blog-three-col">
-                    {blogs.map(blog => <SingleBlog blog={blog} key={blog.id}/>)}
-                </div>
+        <>
+            <h2 className="mb-3">Categories</h2>
+            <div className="blog-category blog-three-col">
+                {blogs.map(blog => <SingleBlog blog={blog} key={blog.id}/>)}
             </div>
-        </div>
+        </>
     )
 }
 
