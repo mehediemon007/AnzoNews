77e2aa4c5cb099d57bd45e98d1df0cdc58cb20d7
mehediemon007 <mehediemon7@gmail.com>
2022-03-08 18:50:02 +0600

    api

--- src/apis/grid-catagory.js
+++ src/apis/grid-catagory.js
@@ -21,6 +21,48 @@ const catData = [
         date : "05 Mar 2022",
         description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
     },
+    {   id:4,
+        image : "nasa-funding.jpg",
+        title : "Nasa Is Ready To Launch Their New Rocket",
+        name : "Sakib Hasan",
+        date : "05 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+    {   id:5,
+        image : "police-van.jpg",
+        title : "Mob Attacked On The Street oF Calefornia on Monday",
+        name : "Sakib Hasan",
+        date : "05 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+    {   id:6,
+        image : "china-trade-war.jpg",
+        title : "China's Trade War With USA Impacts on World Market",
+        name : "Sakib Hasan",
+        date : "05 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+    {   id:7,
+        image : "china-eco.jpg",
+        title : "Chinese Economy Downs In USA After Trade War",
+        name : "Sakib Hasan",
+        date : "05 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+    {   id:8,
+        image : "environment.jpg",
+        title : "Save For Environment Conference Held Among World Leader",
+        name : "Sakib Hasan",
+        date : "05 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+    {   id:9,
+        image : "corona-status.jpg",
+        title : "Worldwide Corona Affected Again With Large Number A Day",
+        name : "Sakib Hasan",
+        date : "05 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
 ]
 
 export default catData;
