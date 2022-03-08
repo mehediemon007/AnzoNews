8adcb2b8aceb9afaca469fd2c75a3307cb0c70bc
mehediemon007 <mehediemon7@gmail.com>
2022-03-08 12:03:18 +0600

    images

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
         <h1>Boss</h1>
+        <i class="uil uil-comments"></i>
+        <img src='images/blogs/ai.jpg' alt='AI' /> 
     </div>
   );
 }
---
+++ src/apis/grid-catagory.js
@@ -0,0 +1,26 @@
+const catData = [
+
+    {
+        image : "boris.jpg",
+        title : "Boris Jonshon Confirms Not To Attend Bexit",
+        name : "Mehedi Emon",
+        date : "03 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+    {
+        image : "australia-assembly.jpg",
+        title : "Australia Shut Down Imigration Policy In Assembly",
+        name : "Mehedi Emon",
+        date : "03 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+    {
+        image : "airindia.jpg",
+        title : "Indian Governmnet Sold Air India To Tata At 80 Crores",
+        name : "Sakib Hasan",
+        date : "05 Mar 2022",
+        description : "It is a long established fact that a reader will bedistracted by the readable content of a when looking at its layout."
+    },
+]
+
+export default catData;
