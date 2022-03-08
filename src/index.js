c4fda3ad7428a7976a32a32f1b5e2b1f51bba330
mehediemon007 <mehediemon7@gmail.com>
2022-03-08 14:00:18 +0600

    sidebar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'owl.carousel/dist/assets/owl.carousel.css';  
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 import App from './App';
 
 ReactDOM.render(
--- src/pages/CategoryGridThree.js
+++ src/pages/CategoryGridThree.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import GridBlogs from '../components/grid-blogs/GridBlogs';
+import SidebarWidget from '../components/sidebar/SidebarWidget';
 
 function CategoryGridThree() {
   return (
     <>
-        <GridBlogs/>
+        <div className="container">
+            <div className="row">
+                <div className="col-xl-9">
+                    <GridBlogs/>
+                </div>
+                <div className="col-xl-3 mb-5 mb-xl-0">
+                    <SidebarWidget/>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
 
-export default CategoryGridThree;
+export default CategoryGridThree
