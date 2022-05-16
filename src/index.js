a76a807b95171bf0ea0c6b76fe3b0a116f95dbd6
mehediemon007 <mehediemon7@gmail.com>
2022-05-16 12:52:43 +0600

    fileFixing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'owl.carousel/dist/assets/owl.carousel.css';
-import "owl.carousel/dist/assets/owl-animate.css";  
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import App from './App';
