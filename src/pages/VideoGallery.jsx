25ccbf470b560d2f1d7ddd862749747e81e3cdd2
mehediemon007 <mehediemon7@gmail.com>
2022-05-18 10:03:14 +0600

    VideoGallery

--- src/pages/VideoGallery.jsx
+++ src/pages/VideoGallery.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React,{useContext, useEffect, useState} from 'react';
+import Footer from '../components/footer/Footer';
+import Toolbar from '../components/header/Toolbar';
+import Navbar from '../components/header/Navbar';
+import VideoModal from '../components/video-blogs/VideoModal';
+import { LangContext } from '../context/LangContext';
+import {Link} from "react-router-dom";
+import data from '../apis/headphone';
 
 const VideoGallery = () => {
+
+    const [videos,setVideos] = useState([]);
+
+
+    const {langShow , setLangShow} = useContext(LangContext);
+
+    useEffect(()=>{
+
+        setVideos(data.newBlogs.featureNews)
+
+    },[])
+
     return (
         <>
-            <h1>Video Gallery</h1>
+            <header className="an-header">
+                <Toolbar langProps={{langShow, setLangShow}}/>
+                <Navbar  langProps={{langShow, setLangShow}}/>
+            </header>
+            <div className="an-ads py-4">
+                <div className="container">
+                    <div className="row px-lg-5">
+                        <img src="images/ads-banner.jpg" alt="Ads Banner"/>
+                    </div>
+                </div>
+            </div>
+            <main className="an-main-content">
+                <div className="an-video-gallery-2 pb-5">
+                    <div className="container">
+                        <div className="an-breadcrumb mb-4 text-end">
+                            <Link to="/" className="breadcrumb-link">Home</Link>
+                            <Link to="/video-gallery" className="breadcrumb-link active">Video Gallery</Link>
+                        </div>
+                        <div className="row g-2 g-sm-3">
+                            {videos.filter(video => video.postType === 'video').map(video => (
+                                <div className="col-6 col-sm-4" key={video.id}>
+                                    <div className="single-video-post">
+                                        <div className="video-post-image position-relative">
+                                            <img src={`images/blogs/${video.image}`} alt={video.alt}/>
+                                            <VideoModal/>
+                                        </div>
+                                        <div className="video-post-info mt-4">
+                                            <Link to="/blog-details"><h4 className="overflow-text">{video.title}</h4></Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <Footer/>
         </>
     )
 }
