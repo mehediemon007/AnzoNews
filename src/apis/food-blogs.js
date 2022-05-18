606dee43a9ace3ce66065108e9072634fc37e879
mehediemon007 <mehediemon7@gmail.com>
2022-05-18 15:22:24 +0600

    Food Data

--- src/apis/food-blogs.js
+++ src/apis/food-blogs.js
@@ -80,8 +80,429 @@ const foodBlogs = {
             category:"Banner"
         },
 
+    ],
+    "foodCategory":[
+        {
+            id:0,
+            image:"chicken.jpg",
+            alt:"Fried chicken",
+            name:"Fried chicken"
+        },
+        {
+            id:1,
+            image:"burger.jpg",
+            alt:"Burger",
+            name:"Burger"
+        },
+        {
+            id:2,
+            image:"pizza.jpg",
+            alt:"Pizza",
+            name:"Pizza"
+        },
+        {
+            id:3,
+            image:"pasta.jpg",
+            alt:"Pasta",
+            name:"Pasta"
+        },
+        {
+            id:4,
+            image:"sushi.jpg",
+            alt:"Sushi",
+            name:"Sushi"
+        },
+        {
+            id:5,
+            image:"food.jpg",
+            alt:"Food",
+            name:"Food"
+        },
+        {
+            id:6,
+            image:"salad.jpg",
+            alt:"Salad",
+            name:"Salad"
+        },
+        {
+            id:7,
+            image:"sandwitch.jpg",
+            alt:"Sanwitch",
+            name:"Sanwitch"
+        },
+        {
+            id:8,
+            image:"potato.jpg",
+            alt:"Potata",
+            name:"Potata",
+            type:"Popular"
+        },
+        {
+            id:9,
+            image:"pizza-lg.jpg",
+            alt:"Pizza",
+            name:"Pizza",
+            type:"Popular"
+        },
+        {
+            id:10,
+            image:"sandwitch.jpg",
+            alt:"Burgers",
+            name:"Burgers",
+            type:"Popular"
+        },
+        {
+            id:11,
+            image:"sandwitch.jpg",
+            alt:"Sandwitch",
+            name:"Sandwitch",
+            type:"Popular"
+        },
+        {
+            id:12,
+            image:"sandwitch.jpg",
+            alt:"Chicken",
+            name:"Chicken",
+            type:"Popular"
+        },
+        {
+            id:13,
+            image:"sandwitch.jpg",
+            alt:"Fish",
+            name:"Fish",
+            type:"Popular"
+        },
+        {
+            id:14,
+            image:"sandwitch.jpg",
+            alt:"Advertise",
+            name:"Advertise",
+            type:"Popular"
+        },
+    ],
+    "recepies":[
+        {
+            id:"0",
+            image:"tacos.jpg",
+            alt:"Taccos",
+            name:"Birria Tacos Recipe",
+            rating:"4.7",
+            tag:"Easy"
+        },
+        {
+            id:"1",
+            image:"chocolate-bomb.jpg",
+            alt:"Chocolate Bombss",
+            name:"Tiktok hot Chocolate Bombs",
+            rating:"4.5",
+            tag:"Desert"
+        },
+        {
+            id:"3",
+            image:"rice.jpg",
+            alt:"Rice",
+            name:"Tiktok Ramen Rice",
+            rating:"4.7",
+            tag:"Chinese"
+        },
+        {
+            id:"4",
+            image:"cake.jpg",
+            alt:"Cheeesecake",
+            name:"Basque Cheeesecake Recipe",
+            rating:"4.5",
+            tag:"Desert"
+        },
+        {
+            id:"4",
+            image:"cake.jpg",
+            alt:"Cheeesecake",
+            name:"Basque Cheeesecake Recipe",
+            rating:"4.5",
+            tag:"Desert"
+        },
+        {
+            id:"5",
+            image:"mac.jpg",
+            alt:"Mac cake",
+            name:"Velveeta Mac And Cheese",
+            rating:"4.5",
+            tag:"Desert"
+        },
+        {
+            id:"6",
+            image:"squash.jpg",
+            alt:"Squash",
+            name:"Honeynut Squash",
+            rating:"4.2",
+            tag:"Vegetable"
+        },
+        {
+            id:"7",
+            image:"bbq.jpg",
+            alt:"Bar-b-q",
+            name:"Ultimate Korean BBQ at Home",
+            rating:"4.5",
+            tag:"Fast Food"
+        },
+        {
+            id:"7",
+            image:"donat.jpg",
+            alt:"Donat",
+            name:"Twisted Bacon",
+            rating:"4.5",
+            tag:"Fast Food"
+        },
+        {
+            id:"8",
+            image:"french-fries.jpg",
+            alt:"French Fries",
+            name:"The Crispiest Smashed Potatoes",
+            rating:"4.6",
+            tag:"Fast Food"
+        },
+        {
+            id:"9",
+            image:"chicken-rice.jpg",
+            alt:"Chicken Rice",
+            name:"Hainanese Chicken Rice:One Pot Chicken and Rice Recipe",
+            rating:"4.6",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Chines Food",
+            type:"Nonveg"
+        },
+        {
+            id:"10",
+            image:"kastu.jpg",
+            alt:"kastu",
+            name:"Chicken katsu :One Pot Chicken katsu is best ever",
+            rating:"3.9",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Japanese Food",
+            type:"Nonveg"
+        },
+        {
+            id:"11",
+            image:"caesar-salad.jpg",
+            alt:"Salad",
+            name:"Chicken Caesar Salad One Pot Chicken and Caesar Salad",
+            rating:"4.6",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Salad",
+            type:"Nonveg"
+        },
+        {
+            id:"12",
+            image:"srimp.jpg",
+            alt:"Srimp",
+            name:"Hainanese Chicken Rice:One Pot Chicken and Rice Recipe",
+            rating:"4.6",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"One Pot",
+            type:"Nonveg"
+        },
+        {
+            id:"13",
+            image:"spagedi.jpg",
+            alt:"Spegadi",
+            name:"Hainanese Chicken Pasta One Pot Chicken and Pasta",
+            rating:"3.6",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Pasta",
+            type:"Nonveg"
+        },
+        {
+            id:"14",
+            image:"beef.jpg",
+            alt:"Beef",
+            name:"Beef Chop Chicken Rice:One Pot Chicken Beef Chop",
+            rating:"4.8",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Beef Recipe",
+            type:"Nonveg"
+        },
+        {
+            id:"15",
+            image:"egg.jpg",
+            alt:"Egg",
+            name:"Hainanese Chicken Rice:One Pot Chicken and Rice Recipe",
+            rating:"4.6",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Chinese Food",
+            type:"Breakfast"
+        },
+        {
+            id:"16",
+            image:"chicken-kastu.jpg",
+            alt:"Chicken Kastu",
+            name:"Chicken katsu :One Pot Chicken katsu is best ever",
+            rating:"3.9",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Japanese Food",
+            type:"Breakfast"
+        },
+        {
+            id:"17",
+            image:"boiled-egg.jpg",
+            alt:"Boiled Egg",
+            name:"Chicken Caesar Salad One Pot Chicken and Caesar Salad",
+            rating:"4.5",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Salad",
+            type:"Breakfast"
+        },
+        {
+            id:"18",
+            image:"honey-cake.jpg",
+            alt:"Honey Braed",
+            name:"Hainanese Chicken Rice:One Pot Chicken and Rice Recipe",
+            rating:"4.6",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"One Pot",
+            type:"Breakfast"
+        },
+        {
+            id:"19",
+            image:"chocolate-vcake.jpg",
+            alt:"Chocolate Cake",
+            name:"Hainanese Chicken Pasta One Pot Chicken and Pasta",
+            rating:"3.6",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Pasta",
+            type:"Breakfast"
+        },
+        {
+            id:"20",
+            image:"beef-kabab.jpg",
+            alt:"Beef kabab",
+            name:"Beef Chop With Nun Bread:One Pot Smoked Beef Chop",
+            rating:"4.8",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Beef Recipe",
+            type:"Breakfast"
+        },
+        {
+            id:"21",
+            image:"cabbage-salad.jpg",
+            alt:"Cabbage Salad",
+            name:"Mixed Pea , Potato With Cabbage Sweet Salad",
+            rating:"4.8",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Chines Food",
+            type:"Veg"
+        },
+        {
+            id:"22",
+            image:"mixed-salad.jpg",
+            alt:"Mixed Salad",
+            name:"Mixed Vaped Vegetable Salad Without Oil For Fatty People",
+            rating:"4.7",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Japanese Food",
+            type:"Veg"
+        },
+        {
+            id:"23",
+            image:"pea-salad.jpg",
+            alt:"Pea Salad",
+            name:"Chestnut Caesar Salad Mix With Vergine Olive Oil",
+            rating:"4.8",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Salad",
+            type:"Veg"
+        },
+        {
+            id:"24",
+            image:"cabbage-olivar-salad.jpg",
+            alt:"Cabbage Olive Salad",
+            name:"Vaped Vegetable  Chicken Roast Without Spice",
+            rating:"4.8",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Vapped",
+            type:"Veg"
+        },
+        {
+            id:"25",
+            image:"salad-with-meat.jpg",
+            alt:"Meat Salad",
+            name:"Regular Mixed Salad With Chicken And Mashroom",
+            rating:"3.8",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Beef Salad",
+            type:"Veg"
+        },
+        {
+            id:"26",
+            image:"tomato-salad.jpg",
+            alt:"Tomato Salad",
+            name:"Varity Vegetable Salad With Pure Creamy Milk Butter",
+            rating:"4.5",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Butter Salad",
+            type:"Veg"
+        },
+        {
+            id:"27",
+            image:"beef-rice.jpg",
+            alt:"Beef Rice",
+            name:"Hainanese Chicken Rice:One Pot Chicken and Rice Recipe",
+            rating:"4.5",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Chinese Food",
+            type:"Easy"
+        },
+        {
+            id:"28",
+            image:"chicken-fry.jpg",
+            alt:"Fried Chicken",
+            name:"Chicken katsu :One Pot Chicken katsu is best ever",
+            rating:"4.5",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Japanese Food",
+            type:"Easy"
+        },
+        {
+            id:"29",
+            image:"beef-slice.jpg",
+            alt:"Beef Slice",
+            name:"Beef Caesar Salad One Pot Chicken and Caesar Salad",
+            rating:"3.5",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Salad",
+            type:"Easy"
+        },
+        {
+            id:"30",
+            image:"puffed-egg.jpg",
+            alt:"Puffed Egg",
+            name:"Hainanese Chicken Rice With Egg:One Pot Chicken and Rice Recipe",
+            rating:"4.1",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Japanese Food",
+            type:"Easy"
+        },
+        {
+            id:"31",
+            image:"beef-bone.jpg",
+            alt:"Meat Bone",
+            name:"Hainanese Chicken Pasta One Pot Chicken and Pasta",
+            rating:"4.5",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Pasta",
+            type:"Easy"
+        },
+        {
+            id:"32",
+            image:"snacks.jpg",
+            alt:"Snacks",
+            name:"Beef Chop Chicken Rice:One Pot Chicken Beef Chop",
+            rating:"4.4",
+            des:"Have you ever had Hainanese chicken rice before? Are you as obsessed with it as I am? I’m guessing the answer is no because.",
+            tag:"Snacks",
+            type:"Easy"
+        },
     ]
 
 }
 
-export default foodBlogs
+export default foodBlogs;
