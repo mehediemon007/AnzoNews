b26c358e45f1adf4525627a2cc72117551553ff2
mehediemon007 <mehediemon7@gmail.com>
2022-05-19 16:50:37 +0600

    sidebar Active Item

--- src/components/sidebar/ProfileSidebar.jsx
+++ src/components/sidebar/ProfileSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const ProfileSidebar = () => {
     return (
@@ -7,11 +7,11 @@ const ProfileSidebar = () => {
             <div className="account-widget bg-white"> 
                 <h5 className="d-flex align-items-center mb-lg-2">My Account <span className="account-menu-toggle d-lg-none"><i className="uil uil-bars mx-2"></i></span></h5>
                 <ul className="account-menu">
-                    <li><Link to="/profile"><i className="uil uil-user-circle"></i>Profile</Link></li>
-                    <li><Link to="/recent-views"><i className="uil uil-postcard"></i>Recent Viewed</Link></li>
-                    <li><Link to="/activity"><i className="uil uil-list-ul"></i>Activities</Link></li>
-                    <li><Link to="/new-post" className="active"><i className="uil uil-folder-plus"></i>New Post</Link></li>
-                    <li><Link to="/forget-password"><i className="uil uil-key-skeleton"></i>Password Change</Link></li>
+                    <li><NavLink to="/profile"><i className="uil uil-user-circle"></i>Profile</NavLink></li>
+                    <li><NavLink to="/recent-views"><i className="uil uil-postcard"></i>Recent Viewed</NavLink></li>
+                    <li><NavLink to="/activity"><i className="uil uil-list-ul"></i>Activities</NavLink></li>
+                    <li><NavLink to="/new-post"><i className="uil uil-folder-plus"></i>New Post</NavLink></li>
+                    <li><NavLink to="/forget-password"><i className="uil uil-key-skeleton"></i>Password Change</NavLink></li>
                     <li><Link to="#" className="logout"><i className="uil uil-sign-out-alt"></i>Logout</Link></li>
                 </ul>
             </div>
